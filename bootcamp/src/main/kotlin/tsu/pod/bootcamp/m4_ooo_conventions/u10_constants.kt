4635e9f3eb2cc775f98796280cbd191fc35814c6
km3ll <kmell@protonmail.com>
2025-04-01 23:33:46 -0500

    Pod 153: Synchronize pod data

--- bootcamp/src/main/kotlin/tsu/pod/bootcamp/m4_ooo_conventions/u10_constants.kt
+++ bootcamp/src/main/kotlin/tsu/pod/bootcamp/m4_ooo_conventions/u10_constants.kt
@@ -1,5 +1,33 @@
 package tsu.pod.bootcamp.m4_ooo_conventions
 
 fun main() {
+
     println("Constants")
+
+    /**
+     * Constants
+     * - 'const' modifier only for primitive types and Strings
+     *   const val answer = 42
+     * - @JvmField
+     *   exposes a property as a field
+     *   @JvmField (no getter is generated)
+     *   val prop = MyClass()
+     */
+    println(" > static")
+    println("   > answer1: " + Computer.answer1)
+    println("   > answer2: " + Computer.answer2)
+    println("   > answer3: " + Computer.answer3)
+
+}
+
+object Computer {
+
+    @JvmStatic
+    val answer1 = 10
+
+    @JvmField
+    val answer2 = 20
+
+    const val answer3 = 30
+
 }
