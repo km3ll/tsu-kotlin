c90a59f4064d8cb11071a97f1988bdb3832e540a
km3ll <kmell@protonmail.com>
2025-04-01 23:06:05 -0500

    Pod 153: Synchronize pod data

--- bootcamp/src/main/kotlin/tsu/pod/bootcamp/m4_ooo_conventions/u08_class_modifiers_two.kt
+++ bootcamp/src/main/kotlin/tsu/pod/bootcamp/m4_ooo_conventions/u08_class_modifiers_two.kt
@@ -1,5 +1,23 @@
 package tsu.pod.bootcamp.m4_ooo_conventions
 
 fun main() {
+
     println("Class Modifiers Two")
+
+    /**
+     * sealed modifier
+     * - restricts class hierarchy
+     * - all subclasses must be located in the same file
+     */
+    println(" > expr: " + eval(Sum(Num(20), Num(30))))
+
+}
+
+sealed class Expr
+class Num(val value: Int) : Expr()
+class Sum(val left: Expr, val right: Expr) : Expr()
+
+fun eval(expr: Expr): Int = when (expr) {
+    is Num -> expr.value
+    is Sum -> eval(expr.left) + eval(expr.right)
 }
