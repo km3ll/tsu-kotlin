b8bd09a37e07cb1b65c8f27f6bfe3ee07311984d
km3ll <kmell@protonmail.com>
2025-04-02 22:51:42 -0500

    Pod 153: Synchronize pod data

---
+++ bootcamp/src/main/kotlin/tsu/pod/bootcamp/m4_ooo_conventions/u13_conventions.kt
@@ -0,0 +1,36 @@
+package tsu.pod.bootcamp.m4_ooo_conventions
+
+fun main() {
+
+    println("Conventions")
+
+    /**
+     * Comparisons
+     * - a > b  | a.comparedTo(b) >  0
+     * - a < b  | a.comparedTo(b) <  0
+     * - a >= b | a.comparedTo(b) >= 0
+     * - a <= b | a.comparedTo(b) <= 0
+     */
+
+    /**
+     * Equality
+     * - s1 == s2 | s1.equals(s2)
+     */
+
+    /**
+     * Elements by index
+     * - get
+     * - set
+     */
+
+    /**
+     * In
+     * - contains
+     */
+
+    /**
+     * .. (start..end)
+     * - start.rangeTo(end)
+     */
+
+}
