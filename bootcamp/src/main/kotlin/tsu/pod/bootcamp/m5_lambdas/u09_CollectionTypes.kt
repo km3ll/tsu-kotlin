4a34d7d92eddb52689a7a270fa8c0ba0cc2d5755
km3ll <kmell@protonmail.com>
2025-04-05 23:57:38 -0500

    Pod 153: Synchronize pod data

--- bootcamp/src/main/kotlin/tsu/pod/bootcamp/m5_lambdas/u09_CollectionTypes.kt
+++ bootcamp/src/main/kotlin/tsu/pod/bootcamp/m5_lambdas/u09_CollectionTypes.kt
@@ -2,22 +2,18 @@ package tsu.pod.bootcamp.m5_lambdas
 
 fun main() {
 
-    println("Nullable Types")
+    println("Collection Types")
 
     /**
-     * Nullable
-     * - @Nullable = Type?
-     * - @NotNull  = Type
+     * List & MutableList
+     * - two interfaces declared in kotlin.collections package
+     * - MutableList extends List
      *
-     * Platform Type (exclamation mark)
-     * - Type!
-     *   - came from Java and its so-called unknown nullability
-     */
-
-    /**
-     * Null Safety
-     * - annotate your Java types
-     * - specify types explicitly
+     * - kotlin.List
+     *   - kotlin.MutableList
+     *     - java.util.ArrayList
+     *
+     * - read-only interfaces improve API
      */
 
 }
