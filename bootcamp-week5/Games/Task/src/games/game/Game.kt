cf7ae2918be0df4e028d79bf2903c727fabfde19
km3ll <kmell@protonmail.com>
2025-04-07 02:35:20 -0500

    Pod 153: Synchronize pod data

--- bootcamp-week5/Games/Task/src/games/game/Game.kt
+++ bootcamp-week5/Games/Task/src/games/game/Game.kt
@@ -1,6 +1,7 @@
 package games.game
 
 import board.Direction
+import games.gameOfFifteen.GameOfFifteenInitializer
 
 interface Game {
 
@@ -13,5 +14,31 @@ interface Game {
     fun processMove(direction: Direction)
 
     operator fun get(i: Int, j: Int): Int?
+
+}
+
+class GameImpl(val initializer: GameOfFifteenInitializer): Game {
+
+    var permutation = mutableListOf<Int>()
+
+    override fun initialize() {
+        permutation.addAll(initializer.initialPermutation)
+    }
+
+    override fun canMove(): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun hasWon(): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun processMove(direction: Direction) {
+        TODO("Not yet implemented")
+    }
+
+    override fun get(i: Int, j: Int): Int? {
+        TODO("Not yet implemented")
+    }
 
 }
--- bootcamp-week5/Games/Task/src/games/gameOfFifteen/GameOfFifteen.kt
+++ bootcamp-week5/Games/Task/src/games/gameOfFifteen/GameOfFifteen.kt
@@ -4,11 +4,11 @@ import board.Direction
 import board.GameBoard
 import board.createGameBoard
 import games.game.Game
+import games.game.GameImpl
 
 /*
  * Implement the Game of Fifteen (https://en.wikipedia.org/wiki/15_puzzle).
  * When you finish, you can play the game by executing 'PlayGameOfFifteen'.
  */
-fun newGameOfFifteen(initializer: GameOfFifteenInitializer = RandomGameInitializer()): Game =
-    TODO()
+fun newGameOfFifteen(initializer: GameOfFifteenInitializer = RandomGameInitializer()): Game = GameImpl(initializer)
 
