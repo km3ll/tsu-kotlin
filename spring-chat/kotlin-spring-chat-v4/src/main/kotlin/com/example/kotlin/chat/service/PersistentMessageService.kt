6a65167f05298e50b10c302b7dc408342c88f25f
km3ll <kmell@protonmail.com>
2025-05-01 15:57:02 -0500

    Pod 153: Synchronize pod data

---
+++ spring-chat/kotlin-spring-chat-v4/src/main/kotlin/com/example/kotlin/chat/service/PersistentMessageService.kt
@@ -0,0 +1,27 @@
+package com.example.kotlin.chat.service
+
+import com.example.kotlin.chat.asDomainObject
+import com.example.kotlin.chat.mapToViewModel
+import com.example.kotlin.chat.repository.MessageRepository
+import org.springframework.context.annotation.Primary
+import org.springframework.stereotype.Service
+
+@Service
+class PersistentMessageService(
+    val messageRepository: MessageRepository,
+) : MessageService {
+    override suspend fun latest(): List<MessageVM> =
+        messageRepository
+            .findLatest()
+            .mapToViewModel()
+
+    override suspend fun after(messageId: String): List<MessageVM> =
+        messageRepository
+            .findLatest(messageId)
+            .mapToViewModel()
+
+    override suspend fun post(message: MessageVM) {
+        messageRepository
+            .save(message.asDomainObject())
+    }
+}
